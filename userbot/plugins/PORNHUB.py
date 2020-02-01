28fd89067456972130adde0840523d836eeb6e29
bhaii126272 <60525032+bhaii126272@users.noreply.github.com>
2020-02-01 18:08:31 +0530

    Update PORNHUB.py

--- userbot/plugins/PORNHUB.py
+++ userbot/plugins/PORNHUB.py
@@ -168,42 +168,6 @@ async def _(event):
             await event.edit(animation_chars[i % 10])
             
             
- @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+ 
 
-async def _(event):
 
-    if event.fwd_from:
-
-        return
-
-    animation_interval = 0.5
-
-    animation_ttl = range(0, 101)
-
-    input_str = event.pattern_match.group(1)
-
-    if input_str == "venom":
-
-        await event.edit(input_str)
-
-        animation_chars = [
-
-            "V_",
-
-            "VE_",
-
-            "VEN_",
-
-            "VENO_",
-            
-            "VENOM🦸_",
-            
-            "VENOM🦸",
-            
-        ]
-
-        for i in animation_ttl:
-
-            await asyncio.sleep(animation_interval)
-
-            await event.edit(animation_chars[i % 10])           
